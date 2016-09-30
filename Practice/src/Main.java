db1233f8d56c192489eb8ca65a79547743b948fd
deenski <jakevendegna@gmail.com>
2016-09-30 08:40:07 -0600

    Remove Practice folder
    Practice folder should not be included, as it is just a shell for
    IntelliJ. Nothing to see here, move along.

--- Practice/src/Main.java
+++
@@ -1,9 +0,0 @@
-public class Main {
-
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
-        System.out.println("My name is Jake");
-        int x = 3;
-        System.out.println(x);
-    }
-}
